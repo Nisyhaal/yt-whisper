b7e821fa08fcdd132a79b83a4620cf333ff921d0
Nisyhaal Tamil <nisyhaal.tamil.v.tamilchelvam@intel.com>
2024-09-05 16:26:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,21 @@ setup(
     author="Miguel Piedrafita",
     install_requires=[
         'yt-dlp',
-        'openai-whisper @ git+https://github.com/openai/whisper.git@main#egg=whisper'
+        'openai-whisper @ git+https://github.com/openai/whisper.git@main#egg=whisper',
+        'openvino>=2024.1.0',
+        'nncf>=2.10.0',
+        'python-ffmpeg<=1.0.16',
+        'moviepy',
+        'transformers',
+        'onnx',
+        'git+https://github.com/huggingface/optimum-intel.git',
+        'peft==0.6.2',
+        'torch>=2.1,<2.4',
+        'torchvision<0.19.0',
+        'git+https://github.com/garywu007/pytube.git',
+        'soundfile',
+        'librosa',
+        'jiwer'
     ],
     description="Generate subtitles for YouTube videos using Whisper",
     entry_points={
